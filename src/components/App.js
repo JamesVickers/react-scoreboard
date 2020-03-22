7cefa21d855a46f355284638becf08d36c0eb2e4
James-Vickers <jvickersdesign@gmail.com>
2020-03-22 14:52:08 +0000

    lifting Counter state up

--- src/components/App.js
+++ src/components/App.js
@@ -7,22 +7,35 @@ class App extends Component {
     players: [
       {
         name: "Guil",
+        score: 0,
         id: 1
       },
       {
         name: "Treasure",
+        score: 0,
         id: 2
       },
       {
         name: "Ashley",
+        score: 0,
         id: 3
       },
       {
         name: "James",
+        score: 0, 
         id: 4
       }
     ]
   };
+
+  handleScoreChange = (delta) => {
+    // this.setState( prevState => ({
+    //   score: prevState.score delta
+    // }));
+    console.log(delta);
+  }
+
+ 
 
   handleRemovePlayer = (id) => {
     this.setState( prevState => {
@@ -44,9 +57,11 @@ class App extends Component {
         {this.state.players.map( player =>
           <Player 
             name={player.name}
+            score={player.score}
             id={player.id}
             key={player.id.toString()} 
-            removePlayer={this.handleRemovePlayer}           
+            changeScore={this.handleScoreChange}      
+            removePlayer={this.handleRemovePlayer}
           />
         )}
       </div>
